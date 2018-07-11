ac0b3a8d8bd95810ace8a2fa0f5858ddbaf1642d
George Gastaldi <gegastaldi@gmail.com>
2018-07-11 14:15:32 -0300

    feat: use Icon and Image

--- src/components/TemplateItem.js
+++ src/components/TemplateItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Card, Image, Label } from 'semantic-ui-react'
+import { Card, Image, Label, Icon } from 'semantic-ui-react'
 
-const TemplateItem = ({ title, icon, tags, description }) => (
+const TemplateItem = ({ title, icon, iconImage, tags, description }) => (
   <Card as="a">
-    {icon && (<Image src={icon} />)}
+    {icon && (<Icon name={icon} size="huge" />)}
+    {iconImage && (<Image src={iconImage} />)}
     <Card.Content>
       <Card.Header>{title}</Card.Header>
       <Card.Description>{description}</Card.Description>
@@ -22,12 +23,14 @@ TemplateItem.propTypes = {
   title: PropTypes.string.isRequired,
   tags: PropTypes.arrayOf(PropTypes.string),
   icon: PropTypes.node,
+  iconImage: PropTypes.node,
   description: PropTypes.string.isRequired,
 };
 
 TemplateItem.defaultProps = {
   tags: [],
   icon: null,
+  iconImage: null
 };
 
 export default TemplateItem;
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -6,8 +6,8 @@ const Create = () => (
   <div className="create-page">
     <Container text>
       <Card.Group itemsPerRow={3}>
-        <TemplateItem title="MicroService" description="A simple MicroService" tags={['backend', 'microservice']} icon="/images/microservices.png"/>
-        <TemplateItem title="CRUD" description="A simple CRUD App" tags={['backend', 'frontend']}/>
+        <TemplateItem title="MicroService" description="A simple MicroService" tags={['backend', 'microservice']} iconImage="/images/microservices.png"/>
+        <TemplateItem title="CRUD" description="A simple CRUD App" tags={['backend', 'frontend']} icon="database"/>
         <TemplateItem title="Vert.x stack" description="Full stack from UI to REST to DB" tags={['backend', 'frontend']}/>
         <TemplateItem title="Messaging" description="AMQ Queue" tags={['messaging', 'amq', 'backend']}/>
         <TemplateItem title="Messaging" description="AMQ Topic" tags={['messaging', 'amq', 'backend']}/>

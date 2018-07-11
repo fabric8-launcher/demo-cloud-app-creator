8dfcd821f0d4b6f70c500c1b7f33dbad396c7082
George Gastaldi <gegastaldi@gmail.com>
2018-07-11 18:12:10 -0300

    chore: changed message

--- src/pages/George.js
+++ src/pages/George.js
@@ -4,7 +4,7 @@ import { Header, Container } from 'semantic-ui-react';
 const George = () => (
   <div className="create-page">
     <Container fluid text>
-      <Header content='Nothing here yet. Move on!'/>
+      <Header content='Nothing here yet. Move along!'/>
     </Container>
   </div>
 );

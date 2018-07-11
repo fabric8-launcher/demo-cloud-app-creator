8d9b467e1e27bb5ddfce05c2d6049989a62d7c85
ia3andy <adamevin@redhat.com>
2018-07-11 18:35:56 +0200

    Add images to items

--- src/components/TemplateItem.js
+++ src/components/TemplateItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Card, Label } from 'semantic-ui-react'
+import { Card, Image, Label } from 'semantic-ui-react'
 
-const TemplateItem = ({ title, tags, description }) => (
+const TemplateItem = ({ title, icon, tags, description }) => (
   <Card as="a">
+    {icon && (<Image src={icon} />)}
     <Card.Content>
       <Card.Header>{title}</Card.Header>
       <Card.Description>{description}</Card.Description>
@@ -20,11 +21,13 @@ const TemplateItem = ({ title, tags, description }) => (
 TemplateItem.propTypes = {
   title: PropTypes.string.isRequired,
   tags: PropTypes.arrayOf(PropTypes.string),
+  icon: PropTypes.node,
   description: PropTypes.string.isRequired,
 };
 
 TemplateItem.defaultProps = {
   tags: [],
+  icon: null,
 };
 
 export default TemplateItem;
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -6,7 +6,7 @@ const Create = () => (
   <div className="create-page">
     <Container text>
       <Card.Group itemsPerRow={3}>
-        <TemplateItem title="MicroService" description="A simple MicroService" tags={['backend', 'microservice']}/>
+        <TemplateItem title="MicroService" description="A simple MicroService" tags={['backend', 'microservice']} icon="/images/microservices.png"/>
         <TemplateItem title="CRUD" description="A simple CRUD App" tags={['backend', 'frontend']}/>
         <TemplateItem title="Vert.x stack" description="Full stack from UI to REST to DB" tags={['backend', 'frontend']}/>
         <TemplateItem title="Messaging" description="AMQ Queue" tags={['messaging', 'amq', 'backend']}/>

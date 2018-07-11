5d947937e4987dc71da04de2709b8489d3337bd5
George Gastaldi <gegastaldi@gmail.com>
2018-07-11 16:55:45 -0300

    chore: cleaning George page

--- src/pages/George.js
+++ src/pages/George.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Container, Card } from 'semantic-ui-react';
-import TemplateItem from '../components/TemplateItem';
+import { Header, Container } from 'semantic-ui-react';
 
 const George = () => (
   <div className="create-page">
     <Container fluid text>
-      <Card.Group itemsPerRow={3}>
-        <TemplateItem title="MicroService" description="A simple MicroService" tags={['backend', 'microservice']} iconImage="/images/microservices.png"/>
-      </Card.Group>
+      <Header content='Nothing here yet. Move on!'/>
     </Container>
   </div>
 );

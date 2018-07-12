cc54242761b374c92a26d8cca7f3b953c3824631
ia3andy <adamevin@redhat.com>
2018-07-12 13:40:42 +0200

    fix: group template component

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './TemplateItem.css';
+import './Template.css';
 
 import { Card, Image, Label, Icon } from 'semantic-ui-react'
 
-const TemplateItem = (props) => (
-  <Card as="a" className={"template-item " + (props.dummy ? "dummy" : "")}>
+const Item = (props) => (
+  <Card as="a" className={`template-item ${props.dummy && 'dummy'}`}>
     {props.icon && (<Icon name={props.icon} size="huge" />)}
     {props.iconImage && (<Image src={props.iconImage} />)}
     <Card.Content>
-      <Card.Header className={(props.dummy ? "dummybold" : "")}>{props.title}</Card.Header>
+      <Card.Header>{props.title}</Card.Header>
       <Card.Description>{props.description}</Card.Description>
     </Card.Content>
     {props.tags.length > 0 && (
@@ -20,20 +20,28 @@ const TemplateItem = (props) => (
   </Card>
 );
 
-TemplateItem.propTypes = {
-  title: PropTypes.string.isRequired,
+Item.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
   tags: PropTypes.arrayOf(PropTypes.string),
   icon: PropTypes.string,
   iconImage: PropTypes.string,
-  description: PropTypes.string.isRequired,
   dummy: PropTypes.bool,
 };
 
-TemplateItem.defaultProps = {
+Item.defaultProps = {
+  title: 'A dummy title',
+  description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
   tags: [],
   icon: null,
   iconImage: null,
   dummy: false
 };
 
-export default TemplateItem;
+const List = (props) => (
+  <Card.Group {...props} />
+);
+
+const Template = { Item, List };
+
+export default Template;
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Container, Card } from 'semantic-ui-react';
-import TemplateItem from '../components/TemplateItem';
+import { Container } from 'semantic-ui-react';
+import Template from '../components/Template';
 
 const Create = () => (
   <div className="create-page">
     <Container text>
-      <Card.Group itemsPerRow={3}>
-        <TemplateItem title="MicroService" description="A simple MicroService" tags={['backend', 'microservice']} iconImage="/images/microservices.png"/>
-        <TemplateItem title="CRUD" description="A simple CRUD App" tags={['backend', 'frontend']} icon="database"/>
-        <TemplateItem title="Vert.x stack" description="Full stack from UI to REST to DB" tags={['backend', 'frontend']}/>
-        <TemplateItem title="Messaging" description="AMQ Queue" tags={['messaging', 'amq', 'backend']}/>
-        <TemplateItem title="Messaging" description="AMQ Topic" tags={['messaging', 'amq', 'backend']}/>
-        <TemplateItem title="Some template" description="The template description" tags={['some', 'tag']} dummy/>
-        <TemplateItem title="Some template" description="The template description" tags={['some', 'tag']} dummy/>
-        <TemplateItem title="Some template" description="The template description" tags={['some', 'tag']} dummy/>
-        <TemplateItem title="Some template" description="The template description" tags={['some', 'tag']} dummy/>
-        <TemplateItem title="Some template" description="The template description" tags={['some', 'tag']} dummy/>
-        <TemplateItem title="Some template" description="The template description" tags={['some', 'tag']} dummy/>
-        <TemplateItem title="Some template" description="The template description" tags={['some', 'tag']} dummy/>
-        <TemplateItem title="Custom" description="Fully custom design" />
-      </Card.Group>
+      <Template.List itemsPerRow={3}>
+        <Template.Item title="MicroService" description="A simple MicroService" tags={['backend', 'microservice']} iconImage="/images/microservices.png"/>
+        <Template.Item title="CRUD" description="A simple CRUD App" tags={['backend', 'frontend']} icon="database"/>
+        <Template.Item title="Vert.x stack" description="Full stack from UI to REST to DB" tags={['backend', 'frontend']}/>
+        <Template.Item title="Messaging" description="AMQ Queue" tags={['messaging', 'amq', 'backend']}/>
+        <Template.Item title="Messaging" description="AMQ Topic" tags={['messaging', 'amq', 'backend']}/>
+        <Template.Item dummy/>
+        <Template.Item dummy/>
+        <Template.Item dummy/>
+        <Template.Item dummy/>
+        <Template.Item dummy/>
+        <Template.Item dummy/>
+        <Template.Item dummy/>
+        <Template.Item title="Custom" description="Fully custom design" />
+      </Template.List>
     </Container>
   </div>
 );

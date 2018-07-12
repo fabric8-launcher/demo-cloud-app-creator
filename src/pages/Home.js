1d8db22d22b1f6672d32593613fd41553020518e
George Gastaldi <gegastaldi@gmail.com>
2018-07-12 18:30:18 -0300

    minor changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => (
       <Header as='h1' content='Think your application. Imagine WE can create it!' />
       <Header as='h2' content='This is a reality...' />
       <Button primary size='huge' as="a" href="/create">Get Started Now<Icon name='right arrow' /></Button>
-      <Divider text="or"/>
+      <Divider horizontal>Or</Divider>
       <Button primary size='huge' as="a" href="/layout2">Try this second demo<Icon name='right arrow' /></Button>
     </Container>
   </div>
--- src/pages/Layout2.js
+++ src/pages/Layout2.js
@@ -17,16 +17,13 @@ export default class Layout2 extends Component {
       </Form>
           <Header content="Let's put your components together"/>
           <Lane title="Applications"/>
-          <Lane title="Integrate with these services"/>
-          <Lane title="Resources"/>
         </Container>
         <Part/>
-        <SelectBox.List itemsPerRow={5} title="foo">
-                <SelectBox.Item title="MicroService" description="A simple MicroService" tags={['backend', 'microservice']} picture="/images/microservices.png"/>
-                <SelectBox.Item title="Config Map" description="A simple CRUD App" tags={['backend', 'frontend']} picture="database" />
-                <SelectBox.Item title="Secret" description="Full stack from UI to REST to DB" tags={['backend', 'frontend']} />
-                <SelectBox.Item title="Messaging" description="AMQ" tags={['messaging', 'amq', 'backend']}/>
-                <SelectBox.Item title="Cache" description="Infinispan" tags={['messaging', 'amq', 'backend']}/>
+        <SelectBox.List itemsPerRow={5}>
+                <SelectBox.Item title="Java" description="A Java application" tags={['backend', 'frontend']} picture="java" />
+                <SelectBox.Item title="JavaScript (Node.js)" description="An application written in JavaScript" tags={['backend', 'frontend']} picture="node" />
+                <SelectBox.Item title="Go" description="An application written in Go" tags={['messaging', 'amq', 'backend']} picture="go"/>
+                <SelectBox.Item title="Ruby" description="Ruby" tags={['messaging', 'amq', 'backend']} picture="ruby"/>
               </SelectBox.List>
       </div>
     )

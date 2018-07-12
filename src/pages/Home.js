5dac8af9b09d67a962d13ba13556c801b02a6484
George Gastaldi <gegastaldi@gmail.com>
2018-07-12 15:44:38 -0300

    Add button to second layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Header, Icon } from 'semantic-ui-react';
+import { Button, Container, Header, Icon, Divider } from 'semantic-ui-react';
 
 const Home = () => (
   <div className="home-page">
@@ -7,6 +7,8 @@ const Home = () => (
       <Header as='h1' content='Think your application. Imagine WE can create it!' />
       <Header as='h2' content='This is a reality...' />
       <Button primary size='huge' as="a" href="/create">Get Started Now<Icon name='right arrow' /></Button>
+      <Divider text="or"/>
+      <Button primary size='huge' as="a" href="/layout2">Try this second demo<Icon name='right arrow' /></Button>
     </Container>
   </div>
 );

f0eb2e8df1f04e4d54d60daa4bb18083424b9533
Tako Schotanus <tako@ceylon-lang.org>
2018-07-31 01:27:41 +0200

    WiP

--- src/t2-design/DiagramWithSteps.js
+++ src/t2-design/DiagramWithSteps.js
@@ -1,12 +1,13 @@
 import classNames from 'classnames';
 import React from 'react';
+import _ from "lodash";
+
 import { Checkbox, Dropdown, Form, Grid, List, Segment, Step } from 'semantic-ui-react';
+
 import DOMRef from "./components/DOMRef";
 import Topology from "./components/Topology";
+
 import './DiagramWithSteps.css';
-
-
-
 
 const empty = {
     nodes: {
@@ -15,7 +16,7 @@ const empty = {
     }
 };
 
-const step0 = {
+const amqLayout = {
     nodes: {
         s1: {type: 'service', name: 'Message Broker: AMQ', services: ['dummy'], suggested: true},
         cm1: {type: 'configmap', name: 'Message Broker Auth', suggested: true},
@@ -25,89 +26,36 @@ const step0 = {
     }
 };
 
-const step1 = {
+const listenerLayout = {
     nodes: {
-        s1: {type: 'service', name: 'Message Broker: AMQ', services: ['dummy']},
-        cm1: {type: 'configmap', name: 'Message Broker Auth'},
         s2: {type: 'service', name: 'Listener: to Database', hasBuildConfig: true, suggested: true},
     },
     edges: {
-        l1: {from: 's1', to: 'cm1'},
         l2: {from: 's2', to: 's1', suggested: true},
     }
 };
 
-const step2 = {
+const dbCreateLayout = {
     nodes: {
-        s1: {type: 'service', name: 'Message Broker: AMQ', services: ['dummy']},
-        cm1: {type: 'configmap', name: 'Message Broker Auth'},
-        s2: {type: 'service', name: 'Listener: to Database', hasBuildConfig: true},
-    },
-    edges: {
-        l1: {from: 's1', to: 'cm1'},
-        l2: {from: 's2', to: 's1'},
-    }
-};
-
-const step3 = {
-    nodes: {
-        s1: {type: 'service', name: 'Message Broker: AMQ', services: ['dummy']},
-        cm1: {type: 'configmap', name: 'Message Broker Auth'},
-        s2: {type: 'service', name: 'Listener: to Database', hasBuildConfig: true},
         s3: {type: 'service', name: 'Database: PostgreSQL', services: ['dummy'], suggested: true},
         cm2: {type: 'configmap', name: 'Database Auth', suggested: true},
         v1: {type: 'storage', name: 'Database Storage', suggested: true},
     },
     edges: {
-        l1: {from: 's1', to: 'cm1'},
-        l2: {from: 's2', to: 's1'},
         l3: {from: 's3', to: 'cm2', suggested: true},
         l4: {from: 's3', to: 'v1', suggested: true},
         l5: {from: 's2', to: 's3', suggested: true},
     }
 };
 
-const step4 = {
+const restLayout = {
     nodes: {
-        s1: {type: 'service', name: 'Message Broker: AMQ', services: ['dummy']},
-        cm1: {type: 'configmap', name: 'Message Broker Auth'},
-        s2: {type: 'service', name: 'Listener: to Database', hasBuildConfig: true},
-        s3: {type: 'service', name: 'Database: PostgreSQL', services: ['dummy']},
-        cm2: {type: 'configmap', name: 'Database Auth'},
-        v1: {type: 'storage', name: 'Database Storage'},
         s4: {type: 'service', name: 'REST API', services: ['dummy'], hasBuildConfig: true, suggested:true},
         r1: {type: 'route', suggested:true},
     },
     edges: {
-        l1: {from: 's1', to: 'cm1'},
-        l2: {from: 's2', to: 's1'},
-        l3: {from: 's3', to: 'cm2'},
-        l4: {from: 's3', to: 'v1'},
-        l5: {from: 's2', to: 's3'},
         l6: {from: 's4', to: 'r1', suggested:true},
         l7: {from: 's4', to: 's3', suggested:true},
-    }
-};
-
-const step5 = {
-    nodes: {
-        s1: {type: 'service', name: 'Message Broker: AMQ', services: ['dummy']},
-        cm1: {type: 'configmap', name: 'Message Broker Auth'},
-        s2: {type: 'service', name: 'Listener: to Database', hasBuildConfig: true},
-        s3: {type: 'service', name: 'Database: PostgreSQL', services: ['dummy']},
-        cm2: {type: 'configmap', name: 'Database Auth'},
-        v1: {type: 'storage', name: 'Database Storage'},
-        s4: {type: 'service', name: 'REST API', services: ['dummy'], hasBuildConfig: true},
-        r1: {type: 'route'},
-    },
-    edges: {
-        l1: {from: 's1', to: 'cm1'},
-        l2: {from: 's2', to: 's1'},
-        l3: {from: 's3', to: 'cm2'},
-        l4: {from: 's3', to: 'v1'},
-        l5: {from: 's2', to: 's3'},
-        l6: {from: 's4', to: 'r1'},
-        l7: {from: 's4', to: 's3'},
     }
 };
 
@@ -117,17 +65,42 @@ class DiagramWithTemplate extends React.Component {
         super(props);
         this.state = {
             selectedItemId: null,
-            showSteps: 0,
-            layout: {
-                nodes: {
-                },
-                edges: {
-                }
-            }
+            steps: [],
+            layout: empty,
         };
     }
 
     scrollIntoView = (props, node) => node && node.scrollIntoView();
+
+    unsuggest = (items) => _.fromPairs(
+        Object.entries(items)
+            .map(([k,v]) => (v.suggested) ? [k,{...v, suggested:false}] : [k,v])
+    );
+
+    pushStep = (layout) => {
+        this.setState((prevState, props) => {
+            let newSteps = [ ...prevState.steps, prevState.layout ];
+            let newLayout = {
+                nodes: { ...this.unsuggest(prevState.layout.nodes), ...layout.nodes },
+                edges: { ...this.unsuggest(prevState.layout.edges), ...layout.edges },
+            };
+            return { steps: newSteps, layout: newLayout };
+        });
+    }
+
+    buttonStep = (layout, title="OK") => (
+        <DOMRef domRef={this.scrollIntoView}>
+            <Step onClick={() => this.pushStep(layout)}>
+                <Step.Content>
+                    <Form>
+                        <Form.Field>
+                            <Form.Button>{title}</Form.Button>
+                        </Form.Field>
+                    </Form>
+                </Step.Content>
+            </Step>
+        </DOMRef>
+    )
 
     startStep = () => (
         <DOMRef domRef={this.scrollIntoView}>
@@ -140,9 +113,11 @@ class DiagramWithTemplate extends React.Component {
         </DOMRef>
     )
 
+    startButtonStep = () => this.buttonStep(amqLayout, "Start");
+
     amqStep = () => (
         <DOMRef domRef={this.scrollIntoView}>
-            <Step onClick={() => this.setState({layout: step1, showSteps: 2})}>
+            <Step onClick={() => this.pushStep(empty)}>
                 <Step.Content>
                     <Step.Title>Message Broker: AMQ</Step.Title>
                     <Step.Description>A message broker based on Red Hat AMQ.</Step.Description>
@@ -216,9 +191,11 @@ class DiagramWithTemplate extends React.Component {
         </DOMRef>
     )
 
+    listenerButtonStep = () => this.buttonStep(listenerLayout, "OK");
+
     dbStep = () => (
         <DOMRef domRef={this.scrollIntoView}>
-            <Step onClick={() => this.setState({layout: step3, showSteps: 4})}>
+            <Step onClick={() => this.pushStep(empty)}>
                 <Step.Content>
                     <Step.Title>Database</Step.Title>
                     <Step.Description>What database do you want to use?</Step.Description>
@@ -275,26 +252,7 @@ class DiagramWithTemplate extends React.Component {
         </DOMRef>
     )
 
-    dbLocalStep = () => (
-        <DOMRef domRef={this.scrollIntoView}>
-            <Step>
-                <Step.Content>
-                    <Step.Title>Create Local Database</Step.Title>
-                    <Step.Description>What database do you want to use?</Step.Description>
-                    <Form>
-                        <Form.Select label="Type" options={[
-                            { key: 'psql', text: 'PostgresQL', value: 'postgresql' },
-                            { key: 'mysql', text: 'mySQL', value: 'mysql' },
-                        ]} placeholder="Database" required />
-                        <Form.Input label="Name" placeholder="Name" required />
-                        <Form.Button label="Schema">Import...</Form.Button>
-                        <Form.Input label="User" placeholder="User" required />
-                        <Form.Input label="Password" type="password" required />
-                    </Form>
-                </Step.Content>
-            </Step>
-        </DOMRef>
-    )
+    dbCreateButtonStep = () => this.buttonStep(dbCreateLayout, "OK");
 
     restStep = () => (
         <DOMRef domRef={this.scrollIntoView}>
@@ -313,6 +271,26 @@ class DiagramWithTemplate extends React.Component {
                             { key: 'com', text: 'Community', value: 'community' },
                             { key: 'rh', text: 'Red Hat', value: 'redhat' },
                         ]} placeholder="Version" required />
+                    </Form>
+                </Step.Content>
+            </Step>
+        </DOMRef>
+    )
+
+    restButtonStep = () => this.buttonStep(restLayout, "OK");
+
+    generateButtonStep = () => (
+        <DOMRef domRef={this.scrollIntoView}>
+            <Step>
+                <Step.Content>
+                    <Step.Title>Generate Application</Step.Title>
+                    <Step.Description>You are now ready to generate your application</Step.Description>
+                    <Form>
+                        <Form.Input label="Application Name" placeholder="Name" value="my-app" required />
+                        <Form.Input label="Maven Artifact" value="booster" required />
+                        <Form.Input label="Maven Version" value="1.0.0-SNAPSHOT" required />
+                        <Form.Input label="Maven Group ID" value="io.openshift" required />
+                        <Form.Button primary icon="cloud upload" labelPosition="right" content="Deploy" onClick={() => this.pushStep(empty)} />
                     </Form>
                 </Step.Content>
             </Step>
@@ -360,42 +338,10 @@ class DiagramWithTemplate extends React.Component {
                         </Grid.Row>
                         <Grid.Row>
                             <Grid.Column>
-                                <a onClick={() => this.setState({layout: empty, showSteps: 0})}>Create new application</a>
+                                <a onClick={() => this.setState({layout: empty, steps: []})}>Create new application</a>
                             </Grid.Column>
                         </Grid.Row>
                     </Grid>
-                </Step.Content>
-            </Step>
-        </DOMRef>
-    )
-
-    buttonStep = (layout, nextStep, title="OK") => (
-        <DOMRef domRef={this.scrollIntoView}>
-            <Step onClick={() => this.setState({layout: layout, showSteps: nextStep})}>
-                <Step.Content>
-                    <Form>
-                        <Form.Field>
-                            <Form.Button>{title}</Form.Button>
-                        </Form.Field>
-                    </Form>
-                </Step.Content>
-            </Step>
-        </DOMRef>
-    )
-
-    generateButtonStep = () => (
-        <DOMRef domRef={this.scrollIntoView}>
-            <Step>
-                <Step.Content>
-                    <Step.Title>Generate Application</Step.Title>
-                    <Step.Description>You are now ready to generate your application</Step.Description>
-                    <Form>
-                        <Form.Input label="Application Name" placeholder="Name" value="my-app" required />
-                        <Form.Input label="Maven Artifact" value="booster" required />
-                        <Form.Input label="Maven Version" value="1.0.0-SNAPSHOT" required />
-                        <Form.Input label="Maven Group ID" value="io.openshift" required />
-                        <Form.Button primary icon="cloud upload" labelPosition="right" content="Deploy" onClick={() => this.setState({showSteps: 7})} />
-                    </Form>
                 </Step.Content>
             </Step>
         </DOMRef>
@@ -406,22 +352,23 @@ class DiagramWithTemplate extends React.Component {
     }
 
     render() {
+        let showSteps = this.state.steps.length;
         return (
             <div className={classNames("t3-design-withsteps")}>
                 <div className={classNames("left-panel")}>
                     <Step.Group fluid vertical>
-                        { this.state.showSteps < 7 && this.startStep() }
-                        { this.state.showSteps == 0 && this.buttonStep(step0, 1, "Start") }
-                        { this.state.showSteps >= 1 && this.state.showSteps < 7 && this.amqStep() }
-                        { this.state.showSteps >= 2 && this.state.showSteps < 7 && this.listenerStep() }
-                        { this.state.showSteps == 2 && this.buttonStep(step2, 3) }
-                        { this.state.showSteps >= 3 && this.state.showSteps < 7 && this.dbStep() }
-                        { this.state.showSteps >= 4 && this.state.showSteps < 7 && this.dbCreateStep() }
-                        { this.state.showSteps == 4 && this.buttonStep(step4, 5) }
-                        { this.state.showSteps >= 5 && this.state.showSteps < 7 && this.restStep() }
-                        { this.state.showSteps == 5 && this.buttonStep(step5, 6) }
-                        { this.state.showSteps >= 6 && this.state.showSteps < 7 && this.generateButtonStep() }
-                        { this.state.showSteps >= 7 && this.infoStep() }
+                        { showSteps < 7 && this.startStep() }
+                        { showSteps == 0 && this.startButtonStep() }
+                        { showSteps >= 1 && showSteps < 7 && this.amqStep() }
+                        { showSteps >= 2 && showSteps < 7 && this.listenerStep() }
+                        { showSteps == 2 && this.listenerButtonStep() }
+                        { showSteps >= 3 && showSteps < 7 && this.dbStep() }
+                        { showSteps >= 4 && showSteps < 7 && this.dbCreateStep() }
+                        { showSteps == 4 && this.dbCreateButtonStep() }
+                        { showSteps >= 5 && showSteps < 7 && this.restStep() }
+                        { showSteps == 5 && this.restButtonStep() }
+                        { showSteps >= 6 && showSteps < 7 && this.generateButtonStep() }
+                        { showSteps >= 7 && this.infoStep() }
                     </Step.Group>
                 </div>
                 <div className={classNames("main-panel")}>
